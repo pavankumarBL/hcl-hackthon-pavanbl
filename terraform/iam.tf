5d145a2869c18b478d5fc62402907c4080ace660
pavankumar <pavankumar.bl@kyndryl.com>
2025-03-29 16:23:39 +0530

    add api gateway permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -77,3 +77,59 @@ resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_policy.arn
   roles      = [aws_iam_role.lambda_role.name]
 }
+
+resource "aws_iam_policy" "lambda_invoke_policy" {
+  name        = "LambdaInvokePolicy"
+  description = "Policy to allow API Gateway to invoke Lambda functions"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "lambda:InvokeFunction"
+        Effect   = "Allow"
+        Resource = "arn:aws:lambda:us-east-1:539935451710:function:appointment-service-lambda"
+      }
+    ]
+  })
+}
+resource "aws_iam_role" "api_gateway_role" {
+  name               = "APIGatewayInvokeLambdaRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_policy_attachment" {
+  role       = aws_iam_role.api_gateway_role.name
+  policy_arn = aws_iam_policy.lambda_invoke_policy.arn
+}
+
+
+resource "aws_api_gateway_method_settings" "method_settings" {
+  rest_api_id = aws_api_gateway_rest_api.api.id       # Replace with your actual API Gateway ID
+  stage_name  = "prod"     # Replace with your actual stage name
+
+  # Specify the method for which you're applying settings (e.g., GET)
+  method_path = "GET"                 # Replace with your actual HTTP method
+
+  # Define at least one setting inside the settings block
+  settings {
+    logging_level = "INFO"
+    metrics_enabled = true
+    data_trace_enabled = true  # Optionally enable data tracing
+    throttling_burst_limit = 5000  # Example: Set burst limit for throttling
+    throttling_rate_limit = 1000   # Example: Set rate limit for throttling
+  }
+}
+
+
