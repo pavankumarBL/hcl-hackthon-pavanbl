a13db867bff6a1587bfb139e4befe77985b91e96
pavankumar <pavankumar.bl@kyndryl.com>
2025-03-29 15:41:53 +0530

    add permission

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -60,6 +60,12 @@ resource "aws_iam_policy" "lambda_policy" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+
 
 resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   name       = "lambda-policy-attachment"
