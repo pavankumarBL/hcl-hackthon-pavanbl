2f78f03888e9ddacff858ba9dfa5f5371ada128c
pavankumar <pavankumar.bl@kyndryl.com>
2025-03-29 16:00:44 +0530

    test image pull

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "patient_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "patients"
+  path_part   = "health"
 }
 
 # Create /appointments resource
