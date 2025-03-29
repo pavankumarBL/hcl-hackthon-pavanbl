ea1b326cd265eac42ef47a3cf3ff42795352b9b4
pavankumar <pavankumar.bl@kyndryl.com>
2025-03-29 14:29:21 +0530

    add ref

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 # Output the ECR repository URI to use in your GitHub Action
 output "patient_service_ecr_uri" {
-  value = aws_ecr_repository.patient_service.repository_url
+  value = aws_ecr_repository.patient_service_ecr.repository_url
 }
 
 output "appointment_service_ecr_uri" {
-  value = aws_ecr_repository.appointment_service.repository_url
+  value = aws_ecr_repository.appointment_service_ecr.repository_url
 }
