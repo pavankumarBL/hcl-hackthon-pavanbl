40411e5d65d059b2074a4983a55fa4a007bac747
pavankumar <pavankumar.bl@kyndryl.com>
2025-03-29 14:27:34 +0530

    create network

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,38 +1,38 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
+# resource "aws_iam_role" "lambda_role" {
+#   name = "lambda_role"
   
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Effect   = "Allow"
-        Sid      = ""
-      },
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         }
+#         Effect   = "Allow"
+#         Sid      = ""
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "Policy for Lambda to interact with AWS services"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["logs:*", "s3:*", "dynamodb:*", "sns:*"]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
+# resource "aws_iam_policy" "lambda_policy" {
+#   name        = "lambda_policy"
+#   description = "Policy for Lambda to interact with AWS services"
+#   policy      = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action   = ["logs:*", "s3:*", "dynamodb:*", "sns:*"]
+#         Effect   = "Allow"
+#         Resource = "*"
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
-  name       = "lambda_policy_attachment"
-  policy_arn = aws_iam_policy.lambda_policy.arn
-  roles      = [aws_iam_role.lambda_role.name]
-}
+# resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
+#   name       = "lambda_policy_attachment"
+#   policy_arn = aws_iam_policy.lambda_policy.arn
+#   roles      = [aws_iam_role.lambda_role.name]
+# }
